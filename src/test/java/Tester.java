94a791c9cdfa1d226b55a1c3914b468786a318ee
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-25 19:09:32 -0800

    Update Tester.java

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -12,17 +12,14 @@ import java.io.*;
 public class Tester {
 
    @Test
-   public void partA () {
-      APLine line1 = new APLine(5,4,-17);
-      double slope1 = line1.getSlope();
-      assertEquals(slope1,-1.25,0.01);
-      boolean onLine1 = line1.isOnLine(5,-2);
-      assertEquals(onLine1,true);
-      APLine line2 = new APLine(-25,40,30);
-      double slope2 = line2.getSlope();
-      assertEquals(slope2,0.625,0.01);
-      boolean onLine2 = line2.isOnLine(5,-2);
-      assertEquals(onLine2,false);
+   public void partA1 () {
+      Sound temp = new Sound();
+      temp.samples = new int [] {40, 2532, 17, -2300, -17, -4000, 2000};
+      //int [] samples = {40, 2532, 17, -2300, -17, -4000, 2000};
+      int result = temp.limitAmplitude(2000);
+      //answer [40, 2000, 17, -2000, -17, -2000, 2000]
+      String failMsg = "Failed - Input=" + temp.samples + ", Expected 3, Output = " + result;
+      assertEquals(failMsg, 3, result);
    }
 
 
